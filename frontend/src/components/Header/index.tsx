9b1159f83b7434b94d7fea80c7fb8f4e07a17a91
Nikita Khitretsov <khitretsovni@pik.ru>
2020-02-14 19:04:23 +0300

    create style file

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { withRouter } from "react-router-dom";
 import { Toolbar } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
+import { useStyles } from './style'
 
 interface Section {
   title: string;
@@ -11,29 +11,9 @@ interface Section {
 }
 
 interface Props {
-  // sections: Array<Section>;
   history: any,
 }
 
-const useStyles = makeStyles(theme => ({
-  toolBarSecondary: {
-    justifyContent: 'space-between',
-    overflowX: 'auto',
-  },
-  toolBarLabel: {
-    flexBasis: 200,
-  },
-
-  toolBarLink: {
-    padding: theme.spacing(1),
-    flexShrink: 0,
-  },
-}));
-
-/**
- * sections оговариваются в этом компоненте, так как шапка не будет менять своих пунктов
- * при переключении страниц - это логика, которая важна только здесь
- */
 const sections = [
   { title: 'О СЕРВИСЕ', url: '/' },
   { title: 'ОЦЕНКА ФИГУРЫ', url: '/' },
@@ -49,7 +29,6 @@ const Header: React.FC<Props> = ({
   const { toolBarSecondary, toolBarLink, toolBarLabel } = useStyles();
 
   return (
-    //play with variant
     <Toolbar component="nav" className={toolBarSecondary} variant="regular">
       <Link color="primary" noWrap key={'Main'} variant="body2" href="#" className={toolBarLabel}>
         НаСтиле
@@ -72,9 +51,4 @@ const Header: React.FC<Props> = ({
   );
 }
 
-/**
- * withRouter предоставляет в компонент объекты, завязанные на работу по переключению
- * страниц. Основной объект - это histiry. В нём есть метод push, которому можно указывать 
- * адрес желаемой для перехода страницы
- */
 export default withRouter(Header)
